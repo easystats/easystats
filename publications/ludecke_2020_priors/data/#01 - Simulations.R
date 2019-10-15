9d39c2151136d4fa41eb213e58f2fd4f762c0c66
Daniel Lüdecke <mail@danielluedecke.de>
2019-10-15 14:00:33 +0200

    update script

--- publications/ludecke_2020_priors/data/#01 - Simulations.R
+++ publications/ludecke_2020_priors/data/#01 - Simulations.R
@@ -1,7 +1,11 @@
 # https://arxiv.org/abs/1708.07487
 library(tidyverse)
 library(strengejacke)
-library(easystats)
+library(insight)
+library(bayestestR)
+library(parameters)
+library(performance)
+library(see)
 library(rstanarm)
 
 generate_data <- function(sample_size = 50, error = 3, effect = 5) {
@@ -37,14 +41,17 @@ compute_models <- function(dat, error, location) {
       cores = 4
     )
   } else {
+    # mimic auto-scaling of prior-scale
+    scale_x2 <- sd(dat$y) * 2.5 / sd(dat$x2)
+    scale_x3 <- sd(dat$y) * 2.5 / sd(dat$x3)
+
     m_stan <- stan_glm(
       y ~ x1 + x2 + x3,
       data = dat,
       family = gaussian(),
       prior = normal(
         location = c(location, 0, 0),
-        # need to check scale with SE from coefficient
-        scale = c(2 * error, .25, 1),
+        scale = c(3 * error, scale_x2, scale_x3),
         autoscale = FALSE
       ),
       refresh = 0,
@@ -88,40 +95,67 @@ set.seed(1207)
 sample_sizes <- seq(50, 300, by = 50)
 locations <- c(NA, 2, 5, 8)
 effect <- 5
-error <- c(.5, 1, 2, 3, 5)
-simulations <- 1:100
+errors <- c(2, 4, 6, 8)
+simulations <- 1:10
 
 result <- data.frame()
 pb <- txtProgressBar(min = 0, max = length(simulations), style = 3)
+total_progress <- length(sample_sizes) * length(locations) * length(errors)
+current_progress <- 0
+
+tstart <- Sys.time()
 
 for (sample_size in sample_sizes) {
   for (location in locations) {
-    for (simulation in simulations) {
-      setTxtProgressBar(pb, simulation)
-      out <- generate_and_process(
-        sample_size = sample_size,
-        error = error,
-        effect = effect,
-        location = location,
-        simulation = simulation
-      )
-      result <- rbind(result, out)
+    for (error in errors) {
+      current_progress <- current_progress + 1
+      for (simulation in simulations) {
+        out <- generate_and_process(
+          sample_size = sample_size,
+          error = error,
+          effect = effect,
+          location = location,
+          simulation = simulation
+        )
+        result <- rbind(result, out)
+        setTxtProgressBar(pb, simulation)
+      }
+      cat("\nFinished N=", sample_size, ", Location=", location, ", Error=", error, "\n")
+      cat("Total progress:", round(100 * current_progress / total_progress), "%", "\n")
     }
-    cat("\nFinished N =", sample_size, "Location =", location, "\n")
   }
 }
 
+cat("\n\nElapsed time for simulations: ")
+cat(format(Sys.time() - tstart))
+cat("\n\n")
+
+attr(result, "elapsed_time") <- format(Sys.time() - tstart)
+
 close(pb)
-save(result, file = "simulations_100.RData")
+
+save(result, file = sprintf(
+  "simulations_%i_%s.RData",
+  length(simulations),
+  gsub(".", "_", make.names(date()), fixed = TRUE)
+))
 
 result$Location[is.na(result$Location)] <- "weakly"
-result$Group <- sprintf("N=%i, Location=%s", result$N, result$Location)
+result$Group <- sprintf("N=%i, Location=%s, Error=%g", result$N, result$Location, result$Error)
+
+theme_set(theme_sjplot2())
 
 ggplot(result, aes(x = as.factor(Location), y = Median)) +
-  geom_boxplot() +
+  geom_boxplot(width = .4) +
   geom_jitter(alpha = .1, width = .1, height = 0, fill = NA) +
-  facet_wrap(~N)
+  facet_wrap(~N) +
+  geom_rug(alpha = .2)
 
+ggplot(result, aes(x = as.factor(N), y = Median)) +
+  geom_boxplot(width = .4) +
+  geom_jitter(alpha = .1, width = .1, height = 0, fill = NA) +
+  facet_wrap(~Location) +
+  geom_rug(alpha = .2)
 
 # tmp <- result %>% filter(Location == "weakly")
 # tmp$Prior[abs(tmp$Prior) > 20] <- NA
