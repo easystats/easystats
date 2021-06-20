cb3e9141a5e629bea504cc7b9d036ae0dc151b1c
mattansb <35330040+mattansb@users.noreply.github.com>
2021-06-20 13:48:47 +0300

    install_suggested can now accept a char vector
    @IndrajeetPatil Split this into two

--- R/install_suggested.R
+++ R/install_suggested.R
@@ -1,8 +1,8 @@
 #' Download all suggested packages
 #'
 #' @param package If \code{NULL}, all suggested packages for all 'easystats'
-#'   packages will be installed. If a specific package is specified, only
-#'   suggested packages for that package will be installed.
+#'   packages will be installed. If a specific package(s) are specified, only
+#'   suggested packages for those package(s) will be installed.
 #'
 #' @details
 #'
@@ -17,93 +17,86 @@
 #' Useful only for its side-effect of installing the needed packages.
 #'
 #' @export
-
 install_suggested <- function(package = NULL) {
+
+  if (is.null(package)) {
+    package <- names(.suggested_pkgs)
+  }
+
+  # unique suggested packages to download
+  pkg_download <- unique(unlist(.suggested_pkgs[package]))
+
+
+  # install only the packages not yet installed
+  installed_packages <- pkg_download %in% rownames(installed.packages())
+
+  if (any(!installed_packages)) {
+    install.packages(pkg_download[!installed_packages])
+  } else {
+    message("All of the suggested packages are already installed :)")
+  }
+}
+
+
+#' @keywords internal_list
+.suggested_pkgs <- list(
   # if you add a new instance of `check_if_installed`, please edit the corresponding entry
   # duplicated entries are fine; we are using unique values anyway
   # please use alphabetical order
 
   # `insight` --------
-  insight_suggested <- c(
+  insight = c(
     "aod", "BayesFactor", "bbmle", "BGGM", "blavaan", "cplm", "clubSandwich",
     "emmeans", "fungible", "gt", "lavaan", "lme4", "mice", "nlme", "rstanarm",
     "sandwich", "statmod"
-  )
+  ),
 
   # `datawizard` --------
-  datawizard_suggested <- c(
+  datawizard = c(
     "boot", "gamm4", "mclust", "multimode", "rstanarm"
-  )
+  ),
 
   # `performance` --------
-  performance_suggested <- c(
+  performance = c(
     "BayesFactor", "bigutilsr", "blavaan", "brms", "car", "cluster",
     "CompQuadForm", "fixest", "glmmTMB", "emmeans", "lavaan", "lme4",
     "loo", "mclust", "Matrix", "mice", "randomForest", "rstanarm",
     "rstantools", "sandwich", "VGAM"
-  )
+  ),
 
 
   # `parameters` --------
-  parameters_suggested <- c(
+  parameters_suggested = c(
     "aod", "BayesFactor", "cAIC4", "cplm", "DRR", "EGAnet", "fastICA", "glmmTMB",
     "emmeans", "lavaan", "lavaSearch2", "MASS", "lme4", "lmerTest", "loo", "Matrix",
     "mice", "nFactors", "projpred", "sandwich"
-  )
+  ),
 
   # `see` --------
-  see_suggested <- c(
+  see = c(
     "emmeans", "ggraph", "grid", "gridExtra", "tidygraph"
-  )
+  ),
 
   # `effectsize` --------
-  effectsize_suggested <- c(
+  effectsize = c(
     "BayesFactor", "boot", "lme4", "lmerTest", "rstanarm", "rstantools"
-  )
+  ),
 
   # `bayestestR` --------
-  bayestestR_suggested <- c(
+  bayestestR = c(
     "BayesFactor", "brms", "bridgesampling", "lme4", "logspline", "MASS",
     "rstan", "rstanarm", "rstantools", "tweedie"
-  )
+  ),
 
   # `correlation` --------
-  correlation_suggested <- c(
+  correlation = c(
     "BayesFactor", "Hmisc", "polycor", "psych", "wdm"
-  )
+  ),
 
   # `report` ----------
   # none
+  report = c(),
 
   # `modelbased` ----------
-  modelbased_suggested <- c("emmeans")
-
-  # unique suggested packages to download
-  if (!is.null(package)) {
-    pkg_download <- switch(package,
-      "insight" = insight_suggested,
-      "datawizard" = datawizard_suggested,
-      "performance" = performance_suggested,
-      "parameters" = parameters_suggested,
-      "see" = see_suggested,
-      "effectsize" = effectsize_suggested,
-      "bayestestR" = bayestestR_suggested,
-      "correlation" = correlation_suggested,
-      "modelbased" = modelbased_suggested,
-      NULL # report
-    )
-  } else {
-    pkg_download <- unique(c(
-      insight_suggested, datawizard_suggested, performance_suggested,
-      parameters_suggested, see_suggested, effectsize_suggested,
-      bayestestR_suggested, correlation_suggested, modelbased_suggested
-    ))
-  }
-
-  # install only the packages not yet installed
-  installed_packages <- pkg_download %in% rownames(installed.packages())
-
-  if (any(installed_packages == FALSE)) {
-    install.packages(pkg_download[!installed_packages])
-  }
-}
+  modelbased = c("emmeans")
+)
