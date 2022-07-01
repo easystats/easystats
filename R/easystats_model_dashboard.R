e4c393b9e03f0d6c95a6c7072b8fd1b88e8b1981
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-07-01 11:52:54 +0200

    Add dashboard infrastructure

---
+++ R/easystats_model_dashboard.R
@@ -0,0 +1,51 @@
+#' @title Generate a regression model summary from `{easystats}`
+#'
+#' @description
+#'
+#' A dashboard containing the following details for the entered regression model:
+#'
+#' - tabular summary of parameter estimates
+#' - a dot-and-whisker plot for parameter estimates
+#' - tabular summary of indices for the quality of model fit
+#' - a collection of models for checking model assumptions
+#' - a text report
+#'
+#' @return
+#' An HTML dashboard.
+#'
+#' @param model A regression model object.
+#' @param output_file A string specifying the file name in `rmarkdown::render()`.
+#' Default is `"easydashboard.html"`.
+#' @param output_dir A string specifying the path to the output directory for
+#'   report in `rmarkdown::render()`. Default is to use the working directory.
+#' @param rmd_dir A string specifying the path to the directory containing the
+#'   RMarkdown template file. By default, package uses the template shipped with
+#'   the package installation (`inst/templates/easydashboard.Rmd`).
+#'
+#' @export
+easystats_model_dashboard <- function(model,
+                                      output_file = "easydashboard.html",
+                                      output_dir = getwd(),
+                                      rmd_dir = system.file("templates/easydashboard.Rmd", package = "easystats")) {
+  insight::check_if_installed("flexdashboard")
+
+  # Render report into html
+  suppressWarnings(
+    rmarkdown::render(
+      input = rmd_dir,
+      output_format = flexdashboard::flex_dashboard(
+        orientation = "columns",
+        vertical_layout = "fill",
+        source_code = "embed"
+      ),
+      output_file = output_file,
+      output_dir = output_dir,
+      intermediates_dir = output_dir,
+      params = list(model = model)
+    )
+  )
+
+  # open dashboard
+  report_path <- path.expand(file.path(output_dir, output_file))
+  utils::browseURL(report_path)
+}
