d5681daf524a9f0eb85af08b5220e4fc3d80aa10
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-01 11:09:13 +0200

    Dashboard providing a collective 'easystats' summary outputs (#254)
    * Add dashboard infrastructure
    
    * oops
    
    * minor
    
    * Update easystats_model_dashboard.Rd
    
    * vertical layout for metrics
    
    * rename to model_dashboard, add further args
    
    * fixes
    
    * Update easydashboard.Rmd
    
    * add more args
    
    * Update model_dashboard.Rd
    
    * docs
    
    * pass new arguments to dashboard
    
    * reference docs
    
    * refactor, use list of args
    
    * typo
    
    * fix args, use md tables
    
    * Update easydashboard.Rmd
    
    * add modebased tab
    
    * Update easydashboard.Rmd
    
    * Update easydashboard.Rmd
    
    * Update easydashboard.Rmd
    
    * Also add report performance
    
    * automatically open links in new tab
    
    * fix printing of predicted values
    
    * rename file and add comment chunk option
    
    * minor
    
    * inform when model not supported
    
    * move computation to their respective chunks
    
    So that error from one chunk doesn't appear everywhere
    
    Co-authored-by: Daniel <mail@danielluedecke.de>
    Co-authored-by: etiennebacher <etienne.bacher@protonmail.com>

---
+++ R/model_dashboard.R
@@ -0,0 +1,79 @@
+#' @title Generate a regression model summary from `{easystats}`
+#'
+#' @description
+#'
+#' A dashboard containing the following details for the entered regression model:
+#'
+#' - tabular summary of parameter estimates
+#' - a dot-and-whisker plot for parameter estimates
+#' - tabular summary of indices for the quality of model fit
+#' - a collection of models for checking model assumptions
+#' - a text report
+#'
+#' @return
+#' An HTML dashboard.
+#'
+#' @param model A regression model object.
+#' @param parameters_args A list of named arguments that are passed down to
+#'   `parameters::model_parameters()`. For further documentation and details
+#'   about the arguments, see [this website](https://easystats.github.io/parameters/reference/model_parameters.html).
+#'   See also 'Examples'.
+#' @param performance_args A list of named arguments that are passed down to
+#'   `performance::model_performance()`. For further documentation and details
+#'   about the arguments, see [this website](https://easystats.github.io/performance/reference/model_performance.html).
+#'   See also 'Examples'.
+#' @param output_file A string specifying the file name in `rmarkdown::render()`.
+#' Default is `"easydashboard.html"`.
+#' @param output_dir A string specifying the path to the output directory for
+#'   report in `rmarkdown::render()`. Default is to use the working directory.
+#' @param rmd_dir A string specifying the path to the directory containing the
+#'   RMarkdown template file. By default, package uses the template shipped with
+#'   the package installation (`inst/templates/easydashboard.Rmd`).
+#'
+#' @examples
+#' if (FALSE) {
+#'   mod <- lm(wt ~ mpg, mtcars)
+#'
+#'   # with default options
+#'   model_dashboard(mod)
+#'
+#'   # customizing {parameters} output: standardize coefficients
+#'   model_dashboard(mod, parameters_args = list(standardize = "refit"))
+#'
+#'   # customizing {performance} output: only show selected performance metrics
+#'   model_dashboard(mod, performance_args = list(metrics = c("AIC", "RMSE")))
+#' }
+#'
+#' @export
+model_dashboard <- function(model,
+                            parameters_args = NULL,
+                            performance_args = NULL,
+                            output_file = "easydashboard.html",
+                            output_dir = getwd(),
+                            rmd_dir = system.file("templates/easydashboard.Rmd", package = "easystats")) {
+  insight::check_if_installed("flexdashboard")
+
+  # render report into HTML
+  suppressWarnings(
+    rmarkdown::render(
+      input = rmd_dir,
+      output_format = flexdashboard::flex_dashboard(
+        orientation = "columns",
+        vertical_layout = "fill",
+        source_code = "embed"
+      ),
+      output_file = output_file,
+      output_dir = output_dir,
+      intermediates_dir = output_dir,
+      params = list(
+        model = model,
+        parameters_args = parameters_args,
+        performance_args = performance_args
+      )
+    )
+  )
+
+  # open dashboard
+  report_path <- path.expand(file.path(output_dir, output_file))
+  utils::browseURL(report_path)
+}
