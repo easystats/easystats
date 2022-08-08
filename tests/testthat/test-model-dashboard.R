d4224f76d2b27170585e480ab12102e687a43f6e
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-08 17:01:25 +0200

    bump needed versions

--- tests/testthat/test-model-dashboard.R
+++ tests/testthat/test-model-dashboard.R
@@ -1,8 +1,8 @@
 test_that("it generates HTML for supported models", {
   withr::with_tempdir(code = {
-   mod <- lm(wt ~ mpg, mtcars)
-   model_dashboard(mod)
-   expect_true(file.exists("easydashboard.html"))
+    mod <- lm(wt ~ mpg, mtcars)
+    model_dashboard(mod)
+    expect_true(file.exists("easydashboard.html"))
   })
 })
 
