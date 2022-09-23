571a71e48f2ba8caeff07310dcbe3502977bedfe
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-23 08:46:27 +0200

    Revert "Update testthat.R"
    This reverts commit 4db6df567f5f449abcb1c3631807f40af7efbe72.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@
 # It is recommended that you do not modify it.
 #
 # Where should you do additional test configuration?
-#
+# Learn more about the roles of various files in:
 # * https://r-pkgs.org/tests.html
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
