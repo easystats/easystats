5b8f964a1d82154059ae099c2b87467a893d99fe
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-09-26 00:58:09 +0530

    Mock unit test interactive dashboard behavior (#382)
    * Mock unit test interactive dashboard behavior
    
    * Update test-model-dashboard.R
    
    * use `{mockery}` instead

--- tests/testthat/test-model-dashboard.R
+++ tests/testthat/test-model-dashboard.R
@@ -13,3 +13,14 @@ test_that("it doesn't fail for unsupported models", {
     expect_true(file.exists(filename))
   })
 })
+
+test_that("it opens HTML in browser in interactive context", {
+  skip_if_not_installed("mockery")
+
+  withr::with_tempdir(code = {
+    mockery::stub(easystats::model_dashboard, "utils::browseURL", NULL)
+
+    expect_null(model_dashboard(NULL, quiet = TRUE, browse_html = TRUE))
+    expect_invisible(model_dashboard(NULL, quiet = TRUE, browse_html = FALSE))
+  })
+})
