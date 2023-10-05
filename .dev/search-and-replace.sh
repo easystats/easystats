16292ca683a94b95e0faacfee7c9559d46957a7a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-10-05 19:40:34 +0200

    add shell script [skip ci]
    Used to automate changes made in commits like https://github.com/easystats/parameters/commit/2fd98103cb6b6c9adc8dc85244b561836ae5e6b1 and

---
+++ .dev/search-and-replace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the search and replace strings
+search_string="# sanity check"
+replace_string="# validation check"
+
+# Find all .R files recursively in the current directory
+find . -type f -name "*.R" -exec sh -c '
+  for file do
+    sed "s/'"$search_string"'/'"$replace_string"'/I" "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  done
+' sh {} +
