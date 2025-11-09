8e8e8c2f41429eb7530d687b1e160b95cccc4e06
Gauarv Chaudhary <chaudharygaurav2004@gmail.com>
2025-11-09 16:01:19 +0530

    Move cite_easystats() from report to easystats package
    This moves the cite_easystats() function from the report package to the
    easystats package, making it more accessible to users. The citation
    vignette has been updated with usage examples showing how to cite
    easystats packages using the new function.
    
    Includes bibliography files and comprehensive tests.
    
    Fixes #448

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
+.support_unicode <- l10n_info()$`UTF-8` |
+  isTRUE(.Options$cli.unicode) |
+  nzchar(Sys.getenv("RSTUDIO_USER_IDENTITY"))
+
 .onAttach <- function(libname, pkgname) {
   # Global kill switches
   if (
